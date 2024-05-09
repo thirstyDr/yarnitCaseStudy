ca67c895a4f2cdc3762798e7227aa0d5b43ae04e
thirstyDr <120259001+thirstyDr@users.noreply.github.com>
2024-05-10 01:09:14 +0530

    Add files via upload

---
+++ yarnitCaseStudy1.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from bs4 import BeautifulSoup
+import requests
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import sent_tokenize
+
+app = Flask(__name__)
+
+@app.route('/get_answer', methods=['POST'])
+def get_answer():
+    data = request.get_json()
+    url = data['url']
+    question = data['question']
+
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    text = soup.get_text()
+    sentences = sent_tokenize(text)
+
+    for sentence in sentences:
+        if question in sentence:
+            return jsonify({'answer': sentence})
+
+    return jsonify({'answer': "I don't know the answer"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
